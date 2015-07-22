e6dfca290500705038fa41e78e509b523aa93b99
tuomasi <brunner.thom@gmail.com>
2015-07-22 17:47:23 +0200

    removed hide header functionality, implemented sticky header on small screen, large nav buttons on small screen, removed tags on small screen

--- js/site.js
+++ js/site.js
@@ -1,4 +1,6 @@
 $(function() {
+
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -12,20 +14,28 @@ $(function() {
     }
   });
 
+
   $("form.wufoo header.info").remove();
 
-  $(".navToggle").on("click", function(){
-    $(".headerToggle").toggle();
-    $(this).toggleClass("fa-chevron-circle-down");
-    $(this).toggleClass("fa-chevron-circle-up");
+
+  $(".nav-toggle").on("click", function(){
+    $(".header__toggle").toggle();
+    $(this).toggleClass("fa-bars");
+    $(this).toggleClass("fa-times");
   });
-  $(".hideHeader").on("click", function(){
-    $(".brand-tagline, .navWrapper").toggle();
+
+
+  // $(".hideHeader").on("click", function(){
+  //   $(".headerToggle").toggle();
+  // });
+
+
+  $(".customise__controls--theme").on("click", function(){
+    $(".header__customise__theme").toggle();
   });
-  $(".theme").on("click", function(){
-    $(".themeContainer").toggle();
-  });
-  $("#applyTheme").on("click", function(){
+
+
+  $("#apply-theme").on("click", function(){
     var theme = $("#theme").find("option:selected").data();
     console.log(theme);
     localStorage.setItem("background-color", theme["backgroundColor"]);
@@ -33,18 +43,22 @@ $(function() {
     applyTheme();
   });
 
+
   if(localStorage.color){
     applyTheme();
   };
+
+
+
   function applyTheme(){
     var theme = {
       "color": localStorage.color,
       "background-color": localStorage["background-color"]
     };
     console.log(theme);
-    $(".headerWrapper").css(theme);
-    $(".brand-title").css("color", theme.color);
-    $(".nav-item a:not(.active)").css("color", theme.color);
+    $(".header").css(theme);
+    $(".header__top__title").css("color", theme.color);
+    $(".nav__item a:not(.active)").css("color", theme.color);
     $(".footer").css(theme);
   };
 });
